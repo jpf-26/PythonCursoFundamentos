5acb9c8cfb428286618e5b24702c44a8de2be0de
João Pedro Furlan <joaopedrofurlan12@gmail.com>
2025-01-27 09:25:47 -0300

    Primeira parte

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu primeiro nome: \n")
+nome2 = input("Digite seu segundo nome: \n")
+
+nometotal = nome + " " + nome2
+
+print(f"Seu ultimo nome é: {nome2} e seu primeiro nome é: {nome}")
+print(nometotal[::-1])
